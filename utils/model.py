22a0820a31c7e3ca64e815de04ba1002cca3d6f0
Javier <jgamtejero@gmail.com>
2020-03-28 10:11:16 +0100

    - Improve documentation of the model - Fix TB callbacks - Unitary test of functions

--- utils/model.py
+++ utils/model.py
@@ -12,21 +12,33 @@ from tensorflow.keras.optimizers import SGD
 
 logger = logging.getLogger(__name__)
 
+
 class FullModel:  # Not Model because it would shadow keras'
     def __init__(self, classes, load_path_PCA=None, load_path_NN=None, n_components=50, layers_NN=[50, 50], lr=0.01,
                  decay=1e-6, momentum=0.9, tensorboard_path=None):
         '''
         This model consists of a PCA and Neural Network. It has all the necessary methods to train and predict all the
         results.
+        FLOW:
+            1. Read data in the form
+            [['stand', '1', '2', '3'],
+             ['walk', '4', '5', '6']]
+
+            2. Create model = FullModel()
+            3. model.prepare_x_y(data)
+            4. model.train_PCA(X)
+            5. X_pca = model.predict_PCA(X)
+            6. model.train_NN(X_pca, Y)
+            7. model.predict(X_test)
         :param classes: list. Different classes the neural network has to be trained in. ['stand', 'walk']?
-        :param load_path_PCA: string. If the PCA model has to be loaded from a location, the path to that location
-        :param load_path_NN: string. If the Neural Network has to be loaded from a location, the path to that location
-        :param n_components: integer. The number of components the PCA has to consider. Also, the input dimension for the
+        :param load_path_PCA: str. If the PCA model has to be loaded from a location, the path to that location
+        :param load_path_NN: str. If the Neural Network has to be loaded from a location, the path to that location
+        :param n_components: int. The number of components the PCA has to consider. Also, the input dimension for the
         Neural Network
         :param layers_NN: list. This list specifies the architecture of the neural network. If [50, 50], two dense layers
         with 50 neurons each will be created. Only used if load_path_NN is None.
         :param lr, decay, momentum: float. Learning rate, decay and momentum for the SGD optimizer in the neural network
-        :param tensorboard_path: string. The path to where the tensorboard logs will be saved. If set to none, no callbacks
+        :param tensorboard_path: str. The path to where the tensorboard logs will be saved. If set to none, no callbacks
         will be used.
         '''
         self.classes = classes
@@ -58,23 +70,39 @@ class FullModel:  # Not Model because it would shadow keras'
             self.save_PCA(savepath)
 
     def to_categorical(self, Y):
-        Y = np.array([self.classes.index(i) for i in Y])
-        return tf.one_hot(Y, depth=self.n_classes)
+        if Y is not None:
+            Y = np.array([self.classes.index(i) for i in Y])
+            return tf.one_hot(Y, depth=self.n_classes)
+        else:
+            return None
 
     def _compile_NN(self, lr, decay, momentum):
         sgd = SGD(lr=lr, decay=decay, momentum=momentum, nesterov=True)
         self.NN.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-    def train_NN(self, X_train, Y_train, batch_size, epochs, X_test=None, Y_test=None, is_one_hot=False, savepath=None):
+    def train_NN(self, X_train, Y_train, batch_size, epochs, X_test=None, Y_test=None, is_one_hot=False, savepath=None,
+                 verbose=0):
+        '''
+        Method to train the neural network.
+        :param X_train, Y_train: ndarray. Training data. X contains the coordinates from PCA (shape (samples, n_components))
+        and Y contains the labels (shape (samples,))
+        :param batch_size: int.
+        :param epochs: int.
+        :param X_test, Y_test: ndarray.
+        :param is_one_hot: bool. Format of the labels. These are converted to a one hot array in order to be fed to the NN.
+        :param savepath: str.
+        :param verbose: int.
+        :return:
+        '''
         if not is_one_hot:
             Y_train = self.to_categorical(Y_train)
+            Y_test = self.to_categorical(Y_test)
 
-        self.NN.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, callbacks=self.callbacks)
+        self.NN.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=batch_size, epochs=epochs,
+                    callbacks=[self.callbacks], verbose=verbose)
 
         if X_test is not None:
             assert Y_test is not None, 'In order to evaluate the model Y_test must be passed to the training function'
-            if not is_one_hot:
-                Y_test = self.to_categorical(Y_test)
             self.NN.evaluate(X_test, Y_test)
 
         if savepath is not None:
@@ -132,25 +160,35 @@ class FullModel:  # Not Model because it would shadow keras'
         logging.debug('Neural network saved to ' + savepath)
 
     def prepare_x_y(self, data):
-        X = data[:, 0]
-        Y = data[1:]
+        Y = data[:, 0]
+        X = data[:, 1:]
         return X, Y
 
     @staticmethod
     def create_callbacks(path):
         return tf.keras.callbacks.TensorBoard(log_dir=path, write_graph=True, histogram_freq=0, update_freq='epoch')
 
+    def get_explained_variance_ratio(self):
+        return sum(self.PCA.explained_variance_ratio_)
 
-'''
-FLOW:
-1. Read data in the form 
-[['jump', '1', '2', '3'],
- ['walk', '4', '5', '6']]
- 
-2. Create model = FullModel()
-3. model.prepare_x_y(data)
-4. model.train_PCA(X)
-5. X_pca = model.predict_PCA(X)
-6. model.train_NN(X_pca, Y)
-7. model.predict(X_test)
-'''
+
+if __name__ == '__main__':
+    from random import randint
+    data = np.random.random((100, 50))
+    classes = ['walk', 'stand']
+    labels = [classes[randint(0, len(classes) - 1)] for _ in range(data.shape[0])]
+    data_labels = np.empty((data.shape[0], data.shape[1] + 1), dtype=object)
+    data_labels[:, 0] = labels
+    data_labels[:, 1:] = data
+
+    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+    tb_path = Path(__file__).parents[1].joinpath('logs/{}/'.format(current_time))
+
+    m = FullModel(classes, tensorboard_path=tb_path, n_components=10)
+
+    X, Y = m.prepare_x_y(data_labels)
+    m.train_PCA(X)
+    X_pca = m.predict_PCA(X)
+    m.train_NN(X_pca, Y, X_test=X_pca, Y_test=Y, batch_size=20, epochs=20, verbose=2)
+
+    m.predict(X)
