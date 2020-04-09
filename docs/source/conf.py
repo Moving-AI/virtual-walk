7e64957a8fa127173f6b4635ddb1b23f5d8971bd
Gonzalo Izaguirre <gontxomde@gmail.com>
2020-04-09 13:19:23 +0200

    Bugfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc'
 ]
-
+master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
